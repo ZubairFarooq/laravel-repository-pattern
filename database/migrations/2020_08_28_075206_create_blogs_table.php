818c1d8eded33731220802f5487ed05dd63c6c0f
zubair-dev <zubairfarooq216@gmail.com>
2020-08-28 13:02:58 +0500

    project init

---
+++ database/migrations/2020_08_28_075206_create_blogs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('description');
+            $table->tinyInteger('is_active')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
